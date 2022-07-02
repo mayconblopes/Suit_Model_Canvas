bf2bbac0e01ccc861c9d6437f9c4fbdfc73da0c5
Lopes, Maycon <lopes.maycon@gmail.com>
2022-07-02 10:30:46 -0300

    implemented del function

--- canvas/urls.py
+++ canvas/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('smc/<str:pk>', views.smc, name="smc"),
-    path('new/', views.new_smc, name="new"),
+    path('new/', views.new_smc, name="new_smc"),
+    path('smc/conf-del/<str:pk>', views.conf_del_smc, name="conf_del_smc"),
+    path('smc/del/<str:pk>', views.del_smc, name="del_smc"),
 
 ]
