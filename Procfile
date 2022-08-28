b21a4cef247cc5449f03a250c12a13dce47624cc
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-28 11:28:03 -0300

    Preparing to deploy on Fly.io

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn smc_project.wsgi
--- smc_project/settings.py
+++ smc_project/settings.py
@@ -21,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY") 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('SECRET_KEY'):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = int(os.environ.get('DEBUG'))
+else:
+    SECRET_KEY = config('SECRET_KEY')
+    DEBUG = int(config('DEBUG'))
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +121,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
