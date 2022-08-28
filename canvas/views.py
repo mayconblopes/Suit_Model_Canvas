481af42182a2b77f8efa360fb9fc8f492ce0a1f4
Lopes, Maycon <lopes.maycon@gmail.com>
2022-08-28 10:40:34 -0300

    Added smc list (all objects) in context of index page.

--- canvas/views.py
+++ canvas/views.py
@@ -3,23 +3,27 @@ from django.shortcuts import redirect
 from canvas.models import SuitModelCanvas
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
-from django.core.paginator import Paginator
-
 
 
 def index(request):
+
     if request.method == 'POST':
         return new_smc(request)
     else:
-        return render(request, 'index.html')
+        smc_list = SuitModelCanvas.objects.all()
+        return render(request, 'index.html', {'smc_list': smc_list})
+
+
 
 def new_smc(request):
     smc = SuitModelCanvas()
     smc.save()
     return redirect(f'/smc/{smc.pk}')
 
+
 def conf_del_smc(request, pk):
     return render(request, 'conf_del_smc.html', {'pk': pk})
+
 
 def del_smc(request, pk):
     smc = get_object_or_404(SuitModelCanvas, pk=pk)
@@ -34,8 +38,8 @@ def smc(request, pk):
 
     # unpack the queryset into a list, then use the inbuilt Python index function to determine smc instance position.
     actual_page = (*qs,).index(smc)
-    
-    #finds the previous and prox (next) smc if exists
+
+    # finds the previous and prox (next) smc if exists
     prev = actual_page
     prox = actual_page
 
@@ -44,13 +48,12 @@ def smc(request, pk):
     except ValueError:
         prev = (qs[actual_page]).pk
         print("Não existe SMC anterior ao atual")
-        
+
     try:
         prox = (qs[actual_page + 1]).pk
     except IndexError:
         prox = (qs[actual_page]).pk
         print("Não existe SMC posterior ao atual")
-
 
     if request.method == "POST":
         smc.pretensao = request.POST.get('pretensao-text') or smc.pretensao
@@ -71,9 +74,9 @@ def smc(request, pk):
         smc.honorarios = request.POST.get('honorarios-text') or smc.honorarios
         smc.demais_despesas = request.POST.get('demais-despesas-text') or smc.demais_despesas
         smc.ementa = request.POST.get('ementa-text') or smc.ementa
-            
+
         smc.save()
-        
+
         if request.htmx:
             # return 204 No Content - HTTP
             return HttpResponse(status=204)
